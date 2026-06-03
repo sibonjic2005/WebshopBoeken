dff7c1990bff6c26be2dc96454944a582925f3e9
Ammar <1071535@hr.nl>
2026-06-03 09:34:18 +0200

    n

--- jest.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('jest').Config} */
-const config = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testMatch: ["**/*.test.ts"],
-  clearMocks: true,
-};
-
-export default config;
